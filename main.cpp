5eaf110a53e7929fc5fc6149d341ba8d04123e12
csabinemeth <ncsaba98@gmail.com>
2022-09-18 13:46:29 +0200

    errors fixed

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int* b = new int[NELEMENTS];  // "int *b" helyett "int* b" kell +  "_" kimaradt
-    std::cout << '1-100 ertekek duplazasa' // <<std::endl; lemaradt 
-        for (int i = 0;) //hiányos for ciklus, "i <= N_ELEMENTS; i++" hiányzik
+    int* b = new int[N_ELEMENTS];  
+    std::cout << "1-100 ertekek duplazasa" << std:endl;  
+    for (int i = 0; i <= N_ELEMENTS; i++) 
         {
             b[i] = i * 2;
         }
-    for (int i = 0; i; i++) //hiányos ciklus, "int i = 0; i <= N_ELEMENTS-1; i++" kell a zárójelbe
+    for (int i = 0; i <= N_ELEMENTS - 1; i++) 
     {
-        std::cout << "Ertek:" //"<< b[i] << std:endl;" lemaradt
+        std::cout << "Ertek:" << b[i] << std:endl; 
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //= 0 lemaradt
+    int atlag = 0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
